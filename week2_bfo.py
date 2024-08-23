987a0c5abb2b849eeae0b1de180be57f7d3e31f6
Kirti Sachdeva <sachdevakirti637@gmail.com>
2024-08-23 14:54:10 +0530

    files updated

--- week2_bfo.py
+++ week2_bfo.py
@@ -7,43 +7,101 @@ Write functions to add records to a file.
 Test and validate file operations.
 '''
 
+
+
 class Student:
-    def __init__(self, name, emailid, rollno, marks):
+    def __init__(self, name, emailid, rollno, mark):
         self.name = name
         self.emailid = emailid
         self.rollno = rollno
-        self.marks = marks
+        self.mark = mark
 
-    def __str__(self):
-        marks_str = ",".join(f"{subject}:{mark}" for subject, mark in self.marks.items())
-        return f"{self.name}->{self.emailid}->{self.rollno}->{marks_str}"
+    def add_record(name, emailid, rollno, mark):
+        with open("student.txt", 'a') as file:
+            d = {}
+            d['name'] = name
+            d['emailid'] = emailid
+            d['rollno'] = rollno
+            d['mark'] = mark
+            file.write(str(d))
+
+            
+
+        b = open("student.txt","r")
+        bb = b.readlines()
+        index = 0
+
+        for x in bb:
+            g = []
+            g = bb[index]+','+"\n"
+            print(g)
+            with open ("student_data.json","a") as file:
+                file.write(g)
+            index+=1
     
-    @classmethod
-    def from_string(cls, data_str):
-        name, emailid, rollno, marks_str = data_str.strip().split("->")
-        marks = dict(subject_mark.split(":") for subject_mark in marks_str.split(","))
-        return cls(name, emailid, rollno, marks)
+#        with open ("student_data.json", "r") as file:
+#            a = json.load(file)
+#            a[rollno] = d
+
+#            with open ("student_data.json","r") as getfile:
+#                json.dump(a, getfile)
+
+
+
+
+
+while True:
+    name = input("Student name : ")
+    emailid = input("Email Id : ")
+    rollno = int(input("Roll Number :"))
+    mark = {}
+
+    while True:
+        subject = input("Enter subject : ")
+        marks = int(input("Enter marks: "))
+        mark[subject] = marks
+        choice = input("Do you want to add more subjects ? [YES/NO] : ")
+        if choice in ["NO","no"]:
+            break
+
+    Student.add_record(name,emailid, rollno, mark)
+    Choice = input("Do you want to enter more student records? [YES/NO] : ")
+    if Choice in ["NO","no"]:
+            break
+
+
+'''
+def add_data():
+    no = input("enter no ")
+    name = input("enter name ")
+    course = input("enter course ")
+
+#    diction = {
+#        "student no ": no,
+#        "student name ": name,
+#        "student course": course
+#    }
+
+    with open("student.txt", 'w') as file:
+            d = {}
+            d['no'] = no
+            d['name'] = name
+            d['course'] = course
+
+            file.write(str(d))
+
+            
+    b = open("student.txt","r")
+    bb = b.readlines()
+    index = 0
+
+    for x in bb:
+        g = []
+        g = bb[index]+','+"\n"
+        print(g)
+        with open ("student_data.json","a") as file:
+            file.write(g)
+        index+=1
     
-def save_students_to_file(students, filename):
-    with open(filename, 'w') as file:
-        for student in students:
-            file.write(str(student)+"\n")
-
-def load_students_from_file(filename):
-    students = []
-    with open(filename,'r') as file:
-        for line in file:
-            students.append(Student.from_string(line))
-    return students
-    
-students = [
-    Student('Kirti','kirti@gmail.com','123',{'Mathematics':96, 'Science':90}),
-    Student('Anvesha','anvesha@gmail.com','124',{'Mathematics':98, 'Science':92}),
-    Student('Anubhav','anubhav@gmail.com','123',{'Mathematics':95, 'Science':99})
-]
-
-save_students_to_file(students, 'students.txt')
-loaded_Students = load_students_from_file('students.txt')
-
-for student in loaded_Students:
-    print(f"Name: {student.name}, Email: {student.emailid}, Roll Number: {student.rollno}, Marks: {student.marks}")
+add_data()
+'''
