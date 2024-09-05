fb581ac240885644ad0eb96cdf1f185ada9b5735
Kirti Sachdeva <sachdevakirti637@gmail.com>
2024-09-05 15:27:29 +0530

    file updated

--- week4_crud.py
+++ week4_crud.py
@@ -7,6 +7,206 @@ Implement search functionality within the CUI.
 Ensure data validation and error handling.
 '''
 
+import json
+class Student:
+    def __init__(self, name, emailid, rollno, mark,student):
+        self.name = name
+        self.emailid = emailid
+        self.rollno = rollno
+        self.mark = mark
+        self.student = student
+
+    def add_record(name, emailid, rollno, mark):
+        with open("student.txt", 'a') as file:
+            d = {}
+            d['name'] = name
+            d['emailid'] = emailid
+            d['rollno'] = rollno
+            d['mark'] = mark
+            file.write(str(d))
+            file.write(",\n")
+
+        with open ("student_data.json","a+") as outfile:
+            json.dump(d, outfile, indent=4)
+
+    def view_records(filename):
+        with open (filename, "r") as file:
+            file_data = file.readlines()
+            for text in file_data:
+                print(text)
+
+    def update_students():
+        rollno = input("Enter roll number: ")
+        with open("student_data.json",'r') as file:
+                file_data = file.read()
+                for student in file_data:
+                    print(student)
+                    if student['rollno'] == rollno:
+                        student['name'] = input("Enter the updated name : ") or student['name']
+                        student['emailid'] = input("Enter the updated emailid : ") or student['emailid']
+        
+                        while True:
+                            subject = input ("Write the subject whose marks need to be updated (or 'done' if none left): ")
+                            if subject.lower() == 'done':
+                                break
+                            markss = input(f"Enter new marks for {subject} : ")
+                            try:
+                                student['marks'][subject] = int(markss)
+                            except ValueError:
+                                print("Enter a valid number.")
+                                continue
+                print("Student record updated successfully.")
+                return
+        print("Student with the mentioned roll number not found.")
+        
+        
+with open ("student_data.json","a") as outfile:
+    outfile.write("[")
+
+student = {}
+while True:
+    name = input("Student name : ")
+    emailid = input("Email Id : ")
+    rollno = int(input("Roll Number :"))
+    mark = {}
+
+    while True:
+        subject = input("Enter subject : ")
+        marks = int(input("Enter marks: "))
+        try:
+            mark[subject] = int(marks)
+        except ValueError:
+            print("Enter a valid number.")
+            continue
+
+        choice = input("Do you want to add more subjects ? [YES/NO] : ")
+        if choice in ["NO","no"]:
+            break
+
+    Student.add_record(name,emailid, rollno, mark)
+    student['name'] = name
+    student['emailid']=emailid
+    student['rollno']=rollno
+    student['marks']=mark
+
+    Choice = input("Do you want to enter more student records? [YES/NO] : ")
+    if Choice in ["NO","no"]:
+        break
+
+with open ("student_data.json","a") as outfile:
+    outfile.write("]")
+
+
+while True:
+    print("Student Records")
+    print("1. Add a Student Record")
+    print("2. View Student Records")
+    print("4. Update Record")
+    print("3. Exit")
+
+    choice = input("Choose an Option: ")
+    if choice == '1':
+        continue
+    elif choice== '2':
+        Student.view_records("student_data.json")
+        print("Viewing all student records.")
+        break
+    elif choice == '3':
+        print("Exiing.")
+        break
+    elif choice == '4':
+            Student.update_students()
+    else:
+        print("Invalid choice.Try Again.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+
 class Student:
     def __init__(self, name, emailid, rollno, marks):
         self.name = name
@@ -68,13 +268,14 @@ def view_students(students):
     for student in students:
         print(f"Name: {student.name}, Email: {student.emailid}, Roll Number: {student.rollno}, Marks: {student.marks}")
 
-def update_students(students):
+
+def update_students(student):
     rollno = input("Enter roll number: ")
-    for student in students:
-        if student.rollno == rollno:
-            print(f"Details of the student are:{student}")
-            student.name = input("Enter the name of the student:") or student.name
-            student.emailid = input("Enter the emailid of the student: ") or student.emailid
+    for students in student:
+        if students['rollno'] == rollno:
+            print(f"Details of the student are:{students}")
+            students['name'] = input("Enter the name of the student:") or students['name']
+            students['emailid'] = input("Enter the emailid of the student: ") or students['emailid']
             while True:
                 subject = input ("Write the subject whose marks need to be updated (or 'done' if none left): ")
                 if subject.lower() == 'done':
@@ -155,3 +356,6 @@ def main():
     
 if __name__ == "__main__":
     main()
+
+
+    '''
